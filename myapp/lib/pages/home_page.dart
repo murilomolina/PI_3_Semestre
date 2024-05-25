80dbc34bb33b29f7ba16067c72d2b30aece35813
maiaJhfB <125679583+maiaJhfB@users.noreply.github.com>
2024-05-25 11:27:56 -0300

    Add files via upload

---
+++ myapp/lib/pages/home_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EurekaMap',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/logo_maua.png',  
+              width: 24,          
+              height: 24,         
+              color: Colors.white, 
+            ),
+            SizedBox(width: 8), // Espaçamento entre a imagem e o texto
+            Text(
+              'EurekaMap',
+              style: TextStyle(color: Colors.white),
+            ),
+          ],
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => LoginScreen()),
+          );
+        },
+        child: Center(
+          child: Text(
+            'Bem Vindo ao EurekaMap',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Login', style: TextStyle(color: Colors.white)),
+      ),
+      body: Center(
+        child: Text(
+          'Tela de Login',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
