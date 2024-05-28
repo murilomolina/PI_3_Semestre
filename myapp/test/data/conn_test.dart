e8106ae7f80980a11c4e16c2319c70fa0b8a526b
Parelho <vinicius.parelho@outlook.com>
2024-05-28 09:01:57 -0300

    Testes usuario  terminados

--- myapp/test/data/conn_test.dart
+++ myapp/test/data/conn_test.dart
@@ -2,6 +2,35 @@ import 'package:mysql_client/mysql_client.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class ConnTest {
+  Future<void> testeInsert() async {
+    try {
+      print("Conectando ao banco de dados");
+
+      final conn = await MySQLConnection.createConnection(
+        host: "127.0.0.1",
+        port: 3306,
+        userName: "root",
+        password: "goodpassword",
+        databaseName: "bdeurekamap",
+      );
+
+      await conn.connect();
+
+      print("Conectado com sucesso");
+
+      var res = await conn.execute("DELETE FROM usuarios");
+      res = await conn.execute(
+          "INSERT INTO usuarios (email, senha) VALUES ('teste@email.com', '123')");
+
+      print("Insert OK");
+
+      print("Fechando conexão");
+      await conn.close();
+    } catch (e) {
+      print("Erro ao conectar: ${e}");
+    }
+  }
+
   Future<void> testeSelect() async {
     try {
       print("Conectando ao banco de dados");
@@ -10,7 +39,7 @@ class ConnTest {
         host: "127.0.0.1",
         port: 3306,
         userName: "root",
-        password: "",
+        password: "goodpassword",
         databaseName: "bdeurekamap",
       );
 
@@ -18,11 +47,20 @@ class ConnTest {
 
       print("Conectado com sucesso");
 
-      var res = await conn.execute("SELECT * FROM usuarios");
+      var res = await conn.execute("SELECT * FROM usuarios", {});
 
+      bool emailEncontrado = false;
       for (final row in res.rows) {
-        print(row.assoc());
+        if (row.colAt(1) == 'teste@email.com') {
+          emailEncontrado = true;
+          break;
+        }
       }
+
+      if (!emailEncontrado) {
+        print("Email teste@email.com não encontrado.");
+      } else
+        print("Email teste@email.com encontrado.");
 
       print("Fechando conexão");
       await conn.close();
@@ -33,6 +71,10 @@ class ConnTest {
 }
 
 void main() {
+  test('ConnTest testeInsert', () async {
+    await ConnTest().testeInsert();
+  });
+
   test('ConnTest testeSelect', () async {
     await ConnTest().testeSelect();
   });
