33eeb677413dd4dd92542f2923d0387024d5b397
Murilo Molina Barone <murilo.m.barone@gmail.com>
2024-06-10 21:19:20 -0300

    correção e implementação da função de cadastro

--- myapp/lib/auth/auth_service.dart
+++ myapp/lib/auth/auth_service.dart
@@ -4,8 +4,7 @@ import 'package:myapp/data/usuario_app.dart';
 class AuthService {
   // Simula o processo de login
   Future<String> login(String email, String senha) async {
-    // TEmpo para fazedr a pesquisa no banco
-    await Future.delayed(const Duration(seconds: 2));
+   
     List<UsuarioApp> usuarios = await consultaUsuariosApp();
 
     for (final usuario in usuarios) {
@@ -17,22 +16,32 @@ class AuthService {
   }
 
   // Simula o processo de cadastro
-  Future<String> signup(String name, String password) async {
-    // TEmpo para fazedr a pesquisa no banco
+  Future<String> signup(String email, String senha) async {
+    // TEmpo para fazedr a pesquisa no banco (para o login não foi necessário!)
     await Future.delayed(const Duration(seconds: 2));
+
+    List<UsuarioApp> usuarios = await consultaUsuariosApp();
+
     // Lógica de cadastro simulada
-    if (name != 'teste@gmail.com') {
-      return ''; // se Cadastro bem-sucedido retorna uma string vazia
+    for (final usuario in usuarios) {
+      if (usuario.email == email) {
+        return 'O nome de usuário já está em uso'; // vazia pouis a função entende que foi bem sucedido
+      }
     }
-    return 'O nome de usuário já está em uso'; // Mensagem de erro
+    try {
+      insereUsuarioApp(email, senha);
+      return '';
+    } catch (e) {
+      return 'Erro ao cadastrar novo usuario!';
+    } 
   }
 
   // Simula o processo de recuperação de senha
-  Future<String> recoverPassword(String name) async {
-    // TEmpo para fazedr a pesquisa no banco
+  Future<String> recoverPassword(String email) async {
+    // TEmpo para fazedr a pesquisa no banco (para o login não foi necessário!)
     await Future.delayed(const Duration(seconds: 2));
     // Lógica de recuperação de senha simulada
-    if (name == 'teste@gmail.com') {
+    if (email == 'teste@gmail.com') {
       return ''; // se Recuperação de senha bem-sucedida retorna uma string vazia
     }
     return 'Nome de usuário não encontrado'; // Mensagem de erro
